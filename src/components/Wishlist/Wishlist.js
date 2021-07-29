50230b19df4bf95dddf34bf0dcc958f8f36dd13e
Jay Rajput <74800982+jay-rajput30@users.noreply.github.com>
2021-07-29 12:53:50 +0530

    refactor: changes useAxios code

--- src/components/Wishlist/Wishlist.js
+++ src/components/Wishlist/Wishlist.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useAuth } from "../../AuthProvider";
 import { useData } from "../../DataProvider";
+import { useAxios } from "../../hooks/useAxios";
 import { useProduct } from "../../ProductProvider";
 // import "./Wish"
 
@@ -10,24 +11,29 @@ const Wishlist = ({ route, setRoute }) => {
   const { dataDispatch } = useData();
   const [fetchWishlist, setFetchWishlist] = useState([]);
   const { userId, wishlistId, authDispatch } = useAuth();
-  useEffect(() => {
-    async function getwishlist() {
-      try {
-        let data = await axios.get(`http://localhost:3001/wishlist/${userId}`);
-        const wishlistProducts = data.data.wishlistItem.products;
-        console.log({ wishlistProducts });
-        setFetchWishlist([...fetchWishlist, ...wishlistProducts]);
-        dataDispatch({ type: "LOAD_WISHLIST", payload: fetchWishlist });
-        authDispatch({
-          type: "SET_WISHLISTID",
-          payload: data.data.wishlistItem._id,
-        });
-      } catch (e) {
-        console.error(e);
-      }
-    }
 
-    getwishlist();
+  const URL = `http://localhost:3001/wishlist/${userId}`;
+  const { data, status, error } = useAxios(URL);
+  useEffect(() => {
+    if (status === "pending") {
+      console.log("data is loading");
+    }
+    // async function getwishlist() {
+    //   try {
+    //     // let data = await axios.get(`http://localhost:3001/wishlist/${userId}`);
+    //     const wishlistProducts = data.data.wishlistItem.products;
+    //     console.log(data.status);
+    //     setFetchWishlist([...fetchWishlist, ...wishlistProducts]);
+    //     dataDispatch({ type: "LOAD_WISHLIST", payload: fetchWishlist });
+    //     authDispatch({
+    //       type: "SET_WISHLISTID",
+    //       payload: data.data.wishlistItem._id,
+    //     });
+    //   } catch (e) {
+    //     console.error(e);
+    //   }
+    // }
+    // getwishlist();
   }, []);
 
   const userWishlist = fetchWishlist.map((item) =>
--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,17 +1,35 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useReducer } from "react";
 
-const useAxios = ({ url }) => {
-  const [networkCall, setNetworkCall] = useState([]);
-  useEffect(() => {
-    function networkCall() {
-      const { data } = axios.get(url);
-      setNetworkCall([...networkCall, data]);
-    }
-    networkCall();
-  }, []);
-
-  return null;
+const reducerFunc = (state, action) => {
+  switch (action.type) {
+    case "success":
+      return { ...state, status: "resolved" };
+    case "error":
+      return { ...state, status: "rejected", error: action.payload };
+    case "started":
+      return { ...state, status: "pending" };
+    default:
+      return state;
+  }
 };
 
-export default useAxios;
+export const useAxios = async (url) => {
+  const [state, axiosDispatch] = useReducer(reducerFunc, {
+    data: null,
+    status: "pending",
+    error: null,
+  });
+  try {
+    const data = await axios.get(url);
+    axiosDispatch({ type: "started" });
+    if (data.status === 200) {
+      axiosDispatch({ type: "success", payload: data });
+    }
+  } catch (err) {
+    axiosDispatch({ type: "error", payload: err });
+    console.error(err);
+  }
+
+  return state;
+};
